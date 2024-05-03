05c6774c88ce3d251286b7f634a2d2516390a3b4
duchoa2302 <165810103+duchoa2302@users.noreply.github.com>
2024-05-03 23:59:05 +0900

    Lesson 1

---
+++ src/main/java/DataType.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class DataType {
+    public static void main(String[] args) {
+      int tuoi = 18;
+      System.out.println("tuoi = " + tuoi);
+      float  pi = 3.141592653589793f;
+      double PI = 3.141592653589793;
+      System.out.println("pi = " + pi);
+      System.out.println("PI = " + PI);
+      String name = "hoa";
+        System.out.println("name = " + name);
+    }
+}
